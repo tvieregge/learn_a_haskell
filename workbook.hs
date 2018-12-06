076a8783ca73647244b6bfc60adc1291913760c3
tvieregge <tmvieregge@gmail.com>
2018-12-06 01:11:43 -0500

    workbook additions to pg 95

--- workbook.hs
+++ workbook.hs
@@ -1,3 +1,6 @@
+import Data.List
+import Data.Char
+
 doubleSmallNum x = if x > 100
                       then x
                       else x*2
@@ -6,3 +9,11 @@ oddResult = [ x | x <- [1,2,3], x <- [1,2,3]]
 
 pg80q = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 pg80q' = length (takeWhile (<1000) $ scanl1 (+) $ map sqrt [1..]) + 1
+
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+isIn' :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn'` haystack = any (isPrefixOf needle) (tails haystack)
+
+pg93 c offset = chr ((ord c) + offset)
