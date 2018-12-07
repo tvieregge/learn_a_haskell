e7a5878500727b02d219e8d45a058d7c65b79c47
tvieregge <tmvieregge@gmail.com>
2018-12-06 22:21:33 -0500

    workbook updates up to modules

--- workbook.hs
+++ workbook.hs
@@ -1,3 +1,4 @@
+import qualified Data.Map as Map
 import Data.List
 import Data.Char
 
@@ -17,3 +18,20 @@ isIn' :: (Eq a) => [a] -> [a] -> Bool
 needle `isIn'` haystack = any (isPrefixOf needle) (tails haystack)
 
 pg93 c offset = chr ((ord c) + offset)
+
+-- no $ needed at end up second line?
+-- phoneBook :: Map.Map String String
+-- phoneBook = Map.fromList $
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("bonnie", "459-2919")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith add xs
+    where add number1 number2 = number1 ++ ", " ++ number2
